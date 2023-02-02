25355479f7870d68e06b450d7be374df44b447e2
Kildyt <kildyt@gmail.com>
2023-02-02 19:39:09 +0100

    Removed unused zone types from instrumentation-document-load

--- plugins/web/opentelemetry-instrumentation-user-interaction/src/internal-types.ts
+++ plugins/web/opentelemetry-instrumentation-user-interaction/src/internal-types.ts
@@ -15,26 +15,6 @@
  */
 
 import { HrTime } from '@opentelemetry/api';
-import { EventName } from './types';
-
-/**
- * Async Zone task
- */
-export type AsyncTask = Task & {
-  eventName: EventName;
-  target: EventTarget;
-  // Allows access to the private `_zone` property of a Zone.js Task.
-  _zone: Zone;
-};
-
-/**
- *  Type for patching Zone RunTask function
- */
-export type RunTaskFunction = (
-  task: AsyncTask,
-  applyThis?: any,
-  applyArgs?: any
-) => Zone;
 
 /**
  * interface to store information in weak map per span
@@ -45,22 +25,3 @@ export interface SpanData {
   taskCount: number;
   currentLocation?: string;
 }
-
-/**
- * interface to be able to check Zone presence on window
- */
-export interface WindowWithZone {
-  Zone: ZoneTypeWithPrototype;
-}
-
-/**
- * interface to be able to use prototype in Zone
- */
-interface ZonePrototype {
-  prototype: any;
-}
-
-/**
- * type to be  able to use prototype on Zone
- */
-export type ZoneTypeWithPrototype = ZonePrototype & Zone;
